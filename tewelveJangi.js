b8f1ea417e1e70a6a5b453e4fd1ba7f1fab0a4d1
cola314 <66579357+cola314@users.noreply.github.com>
2020-07-25 12:45:00 +0900

    Update tewelveJangi.js

--- tewelveJangi.js
+++ tewelveJangi.js
@@ -1,6 +1,6 @@
 const NOTHING = 0;
 const PLAYER1 = 1;
-const PLYAER2 = 2;
+const PLAYER2 = 2;
 
 const BOARD = 0;
 const HAVING = 1;
@@ -13,14 +13,15 @@ const Hu = 4;
 const Mu = 5;
 
 const SELECTED = 1;
-const MOVED    = 2;
+const MOVED = 2;
 
 const MAL_DIR = [
-    [[1, -1], [1, 0], [1, 1], [0, -1], [0, 1], [-1, 1], [-1, 0], [-1, 1]],
+    [[1, -1], [1, 0], [1, 1], [0, -1], [0, 1], [-1, 1], [-1, 0], [-1, -1]],
     [[1, 1], [1, -1], [-1, 1], [-1, -1]],
     [[1, 0], [-1, 0], [0, 1], [0, -1]],
     [[-1, 0]],
-    [[-1, -1], [-1, 0], [-1, 1], [0, 1], [0, -1], [1, 0]]
+    [[-1, -1], [-1, 0], [-1, 1], [0, 1], [0, -1], [1, 0]],
+    []
 ];
 
 function MAL(player, type, pos, highlighted) {
@@ -49,18 +50,18 @@ GAME.prototype.init_game = function () {
     for (var i = 0; i < 4; i++) {
         this.board[i] = [null, null, null];
     }
-    board[0][0] = new MAL(PLAYER2, Jang, new POS(BOARD, 0, 0), false);
-    board[0][1] = new MAL(PLYAER2, Wang, new POS(BOARD, 0, 1), false);
-    board[0][2] = new MAL(PLYAER2, Wang, new POS(BOARD, 0, 2), false);
-    board[1][0] = new MAL(NOTHING, Mu, new POS(BOARD, 1, 0), false);
-    board[1][1] = new MAL(PLYAER2, Ja, new POS(BOARD, 1, 1), false);
-    board[1][2] = new MAL(NOTHING, Mu, new POS(BOARD, 1, 2), false);
-    board[2][0] = new MAL(NOTHING, Mu, new POS(BOARD, 2, 0), false);
-    board[2][1] = new MAL(PLYAER1, Ja, new POS(BOARD, 2, 1), false);
-    board[2][2] = new MAL(NOHTING, Mu, new POS(BOARD, 2, 2), false);
-    board[3][0] = new MAL(PLAYER1, Sang, new POS(BOARD, 3, 0), false);
-    board[3][1] = new MAL(PLYAER1, Wang, new POS(BOARD, 3, 1), false);
-    board[3][2] = new MAL(PLYAER1, Jang, new POS(BOARD, 3, 2), false);
+    this.board[0][0] = new MAL(PLAYER2, Jang, new POS(BOARD, 0, 0), false);
+    this.board[0][1] = new MAL(PLAYER2, Wang, new POS(BOARD, 0, 1), false);
+    this.board[0][2] = new MAL(PLAYER2, Sang, new POS(BOARD, 0, 2), false);
+    this.board[1][0] = new MAL(NOTHING, Mu, new POS(BOARD, 1, 0), false);
+    this.board[1][1] = new MAL(PLAYER2, Ja, new POS(BOARD, 1, 1), false);
+    this.board[1][2] = new MAL(NOTHING, Mu, new POS(BOARD, 1, 2), false);
+    this.board[2][0] = new MAL(NOTHING, Mu, new POS(BOARD, 2, 0), false);
+    this.board[2][1] = new MAL(PLAYER1, Ja, new POS(BOARD, 2, 1), false);
+    this.board[2][2] = new MAL(NOTHING, Mu, new POS(BOARD, 2, 2), false);
+    this.board[3][0] = new MAL(PLAYER1, Sang, new POS(BOARD, 3, 0), false);
+    this.board[3][1] = new MAL(PLAYER1, Wang, new POS(BOARD, 3, 1), false);
+    this.board[3][2] = new MAL(PLAYER1, Jang, new POS(BOARD, 3, 2), false);
 }
 
 GAME.prototype.set_turn = function (player) {
@@ -68,12 +69,12 @@ GAME.prototype.set_turn = function (player) {
 }
 
 GAME.prototype.get_board = function () {
-    return this.board();
+    return this.board;
 }
 
 GAME.prototype.get_having = function (player) {
     if (player == PLAYER1) return this.player1;
-    if (player == PLAYER2) return this.palyer2;
+    else if (player == PLAYER2) return this.player2;
     return null;
 }
 
@@ -86,7 +87,7 @@ GAME.prototype.button_click = function (player, pos) {
             for (let i = 1; i < 4; i++) {
                 for (let j = 0; j < 3; j++) {
                     if (this.board[i][j].type == Mu) {
-                        this.boad[i][j].highlighted = true;
+                        this.board[i][j].highlighted = true;
                     }
                 }
             }
@@ -97,7 +98,7 @@ GAME.prototype.button_click = function (player, pos) {
             for (let i = 0; i < 3; i++) {
                 for (let j = 0; j < 3; j++) {
                     if (this.board[i][j].type == Mu) {
-                        this.boad[i][j].highlighted = true;
+                        this.board[i][j].highlighted = true;
                     }
                 }
             }
@@ -105,49 +106,59 @@ GAME.prototype.button_click = function (player, pos) {
         return SELECTED;
     }
     else if (pos.from == BOARD) {
-        if (this.board[pos.y][pos.x].type.highlighted) {
+        if (this.board[pos.y][pos.x].highlighted) {
             if (this.board[pos.y][pos.x].type == Wang) {
                 this.state = player;
             }
             else if (this.board[pos.y][pos.x].type != Mu) {
-                let this_player = (player == PLYAER1 ? player1 : player2);
+                let this_player = (player == PLAYER1 ? this.player1 : this.player2);
                 for (let i = 0; i < 6; i++) {
-                    if(this_player[i] == null) {
+                    if (this_player[i] == null) {
                         this_player[i] = new MAL(player, this.board[pos.y][pos.x].type, new POS(HAVING, i, 0), false);
                         break;
                     }
                 }
             }
-            this.board[this.selected.y][this.selected.x] = new MAL(NOTHING, Mu, new POS(BOARD, this.selected.y, this.selected.x), false);
+
+            if (this.selected.pos.from == BOARD) this.board[this.selected.pos.y][this.selected.pos.x] = new MAL(NOTHING, Mu, new POS(BOARD, this.selected.pos.y, this.selected.pos.x), false);
+            else if (this.selected.pos.from == HAVING) {
+                let this_player = (player == PLAYER1 ? this.player1 : this.player2);
+                for (let i = this.selected.pos.y; i < 5; i++) {
+                    if(this_player[i + 1] != null) {
+                        this_player[i] = new MAL(player, this_player[i+1].type, new POS(HAVING, i, 0), false);
+                    }
+                    else this_player[i] = null;
+                }
+                this_player[5] = null;
+            }
             this.board[pos.y][pos.x] = new MAL(player, this.selected.type, new POS(BOARD, pos.y, pos.x), false);
-            if(this.board[pos.y][pos.x].type == Ja && (player == PLAYER1 && pos.y == 0 || player == PLAYER2 && pos.y == 3)) {
+            if (this.board[pos.y][pos.x].type == Ja && (player == PLAYER1 && pos.y == 0 || player == PLAYER2 && pos.y == 3)) {
                 this.board[pos.y][pos.x].type = Hu;
             }
             clear_highlighted(this.board);
 
-            if(player == PLAYER1) {
-                for(let i=0; i<3; i++) {
-                    if(this.board[3][i].type == Wang && this.board[3][i].player == PLAYER2) {
+            if (player == PLAYER1) {
+                for (let i = 0; i < 3; i++) {
+                    if (this.board[3][i].type == Wang && this.board[3][i].player == PLAYER2) {
                         this.state = PLAYER2;
                     }
                 }
             }
-            else if(player == PLAYER2) {
-                for(let i=0; i<3; i++) {
-                    if(this.board[0][i].type == Wang && this.board[0][i].player == PLAYER2) {
+            else if (player == PLAYER2) {
+                for (let i = 0; i < 3; i++) {
+                    if (this.board[0][i].type == Wang && this.board[0][i].player == PLAYER1) {
                         this.state = PLAYER1;
                     }
                 }
             }
-
             return MOVED;
         }
-        else {
+        else if (this.board[pos.y][pos.x].player == player) {
             this.selected = this.board[pos.y][pos.x];
             clear_highlighted(this.board);
             for (let i = 0; i < MAL_DIR[this.selected.type].length; i++) {
-                let y = pos.y + (player == PLYAER1 ? 1 : -1) * MAL_DIR[this.selected.type][0];
-                let x = pos.x + MAL_DIR[this.selected.type][1];
+                let y = pos.y + (player == PLAYER1 ? 1 : -1) * MAL_DIR[this.selected.type][i][0];
+                let x = pos.x + MAL_DIR[this.selected.type][i][1];
                 if (0 <= x && x < 3 && 0 <= y && y < 4 && this.board[y][x].player != player) {
                     this.board[y][x].highlighted = true;
                 }
